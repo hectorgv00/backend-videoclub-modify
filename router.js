eda8fb482ce180690b50ed908e49ff8b88aeb9e0
hector.gv00 <hector.gv00@gmail.com>
2022-12-02 18:06:19 +0100

    Deploy commit

--- router.js
+++ router.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const moviesRoutes = require('./routes/moviesRoutes.js');
-// const seriesRoutes = require('./routes/seriesRoutes.js');
+const seriesRoutes = require('./routes/seriesRoutes.js');
 const userRoutes = require('./routes/userRoutes.js');
 const loansRoutes = require('./routes/loansRoutes.js');
-// const userFindRoutes = require('./routes/userFindRoutes');
 const { authBearerMiddleware, isValidRoleAdmin } = require("./middleware/auth.middleware")
 
 
 
 router.use('/movies', moviesRoutes);
-// router.use('/series', seriesRoutes);
+router.use('/series', seriesRoutes);
 router.use('/users', userRoutes);
 router.use('/loans', loansRoutes);
 
