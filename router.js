9ffdd7622034a0a031a50603da0be943c679705c
hector.gv00 <hector.gv00@gmail.com>
2022-12-02 18:02:18 +0100

    ccambios duros deploy

--- router.js
+++ router.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const moviesRoutes = require('./routes/moviesRoutes');
-const seriesRoutes = require('./routes/seriesRoutes');
-const userRoutes = require('./routes/userRoutes');
-const loansRoutes = require('./routes/loansRoutes');
+const moviesRoutes = require('./routes/moviesRoutes.js');
+// const seriesRoutes = require('./routes/seriesRoutes.js');
+const userRoutes = require('./routes/userRoutes.js');
+const loansRoutes = require('./routes/loansRoutes.js');
 // const userFindRoutes = require('./routes/userFindRoutes');
 const { authBearerMiddleware, isValidRoleAdmin } = require("./middleware/auth.middleware")
 
 
 
 router.use('/movies', moviesRoutes);
-router.use('/series', seriesRoutes);
+// router.use('/series', seriesRoutes);
 router.use('/users', userRoutes);
 router.use('/loans', loansRoutes);
 
