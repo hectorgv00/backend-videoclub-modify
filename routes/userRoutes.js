86db6385dced948bc3f55fc446c648334aa89bbb
hector.gv00 <hector.gv00@gmail.com>
2022-12-02 17:37:28 +0100

    cambios deploy

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const usersControllers = require('../controllers/usersControllers');
+const usersControllers = require('../controllers/UsersControllers');
 const { authBearerMiddleware, isAdmin } = require("../middleware/auth.middleware")
 
 
